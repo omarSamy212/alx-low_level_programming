5b32bb22f95e524e7c77715797fa76ba1a49b6c6
omarSamy212 <omarsamy0211@gmail.com>
2023-02-17 07:29:33 -0800

    TASK8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Write a program that prints all single digit
+ * numbers of base 10 starting from 0 using putchar'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
