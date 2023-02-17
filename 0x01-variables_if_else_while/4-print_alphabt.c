745248ef07c455b247f81fffd024d6910651006d
omarSamy212 <omarsamy0211@gmail.com>
2023-02-17 05:08:00 -0800

    TASK4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print alphabet in lower case except q,e'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' || i == 'e')
+			continue;
+		putchar(i);
+	}
+	return (0);
+}
