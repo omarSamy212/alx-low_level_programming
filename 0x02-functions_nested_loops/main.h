cdca9d9f8a283a77184aea3b1da46764b6b2f9a1
omarSamy212 <omarsamy0211@gmail.com>
2023-02-22 08:02:44 -0800

    headerfile

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,20 +10,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-/**
- * print_alphabet - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-void print_alphabet(void)
-{
-	char i;
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-}
